3b390b6720e4a363692c4d31bae2e97ce819f022
smonke <sonja.moenkemeyer@gmx.de>
2017-06-17 12:50:00 +0200

    setup seperate and development envs

--- playground/mongodb-delete.js
+++ playground/mongodb-delete.js
@@ -1,42 +1,21 @@
-//const MongoClient = require('mongodb').MongoClient;
-const {MongoClient, ObjectID} = require('mongodb');
+const {ObjectID} = require('mongodb');
 
-// var user = {name: 'Sonja', age: 35};
-// var {name} = user;
-// console.log(name);
+const {mongoose} = require('./../server/db/mongoose');
+const {Todo} = require('./../server/models/todo');
+const {User} = require('./../server/models/user');
 
-MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
-    if (err) {
-        console.log('Unable to connect to MongoDB server');
-    }
-    console.log('Connected to the MongoDB server');
+// Todo.remove({}).then((result) => {
+//     console.log(result);
+// });
 
-    // deleteMany
-    // db.collection('Todos').deleteMany({text: 'Eat lunch'}).then((result) => {
-    //     console.log(result);
-    // });
-    
-    // deleteOne
-    // db.collection('Todos').deleteOne({text: 'Eat lunch'}).then((result) => {
-    //     console.log(result);
-    // });
+// Todo.findOneAndRemove
+// Todo.findByIdAndRemove
 
-
-    // findOneAndDelete
-    // db.collection(Todos).findOneAndDelete({completed: false}).then((result) => {
-    //     console.log(result)
-    // });
-
-    // deleteMany User sonja
-    db.collection('Users').deleteMany({name: 'Jen'}).this((result) => {
-        console.log(result);
-    });
-
-    // dinfOneAndDelete by id
-    // db.collection('Users').findOneAndDelete({_id: '593447d2f4ece5df3797493a'}).this((result) => {
-    //     console.log(result);
-    // });
-
-    db.close();
+Todo.findOneAndRemove({_id: '5944fe975fa9e35addf26d3f'}).then((todo) => {
+    console.log(todo);
 });
+
+// Todo.findByIdAndRemove('5944fe975fa9e35addf26d3f').then((todo) => {
+//     console.log(todo);
+// });
 
