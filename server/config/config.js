74efbbd78ab18fdd4652ae920dc7deef5049777c
smonke <sonja.moenkemeyer@gmx.de>
2017-06-25 13:38:34 +0200

    check env variables

--- server/config/config.js
+++ server/config/config.js
@@ -10,11 +10,6 @@ if (env === 'development' || env === 'test') {
   })
 };
 
-if(env === 'ci') {
-  process.env.PORT = 3000;
-  process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoApp';
-}
-
 // if(env === 'development') {
 //   process.env.PORT = 3000;
 //   process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoApp';
