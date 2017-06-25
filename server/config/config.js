f594acb575e87e985fcbbb455930fb050372c52a
smonke <sonja.moenkemeyer@gmx.de>
2017-06-25 13:32:03 +0200

    fix config.js

--- server/config/config.js
+++ server/config/config.js
@@ -7,5 +7,18 @@ if (env === 'development' || env === 'test') {
 
   Object.keys(envConfig).forEach((key) => {
     process.env[key] = envConfig[key];
-  });
+  })
+};
+
+if(env === 'ci') {
+  process.env.PORT = 3000;
+  process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoApp';
 }
+
+// if(env === 'development') {
+//   process.env.PORT = 3000;
+//   process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoApp';
+// } else if (env === 'test') {
+//   process.env.PORT = 3000;
+//   process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoAppTest';
+// }
